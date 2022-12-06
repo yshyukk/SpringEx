a5749eb1f82941565b9f56554f2985e98c413947
윤상혁 <01092103780@naver.com>
2022-12-06 23:16:50 +0900

    기본세팅완료

---
+++ SpringMVCXml/src/main/java/kr/co/fusion/controller/HomeController.java
@@ -0,0 +1,18 @@
+package kr.co.fusion.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	
+	//return 값에 jsp경로
+	public String home() {
+		System.out.println("home");
+		return "index";
+	}
+	
+	
+}
---
+++ SpringMVCXml/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>Hello.spring MVC</h1>
+	<img src="images/spring.svg"/>
+	<img src="images/이미지저장.png"/>
+</body>
+</html>
